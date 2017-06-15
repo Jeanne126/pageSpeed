d6eafec7038cfd6b7a92489481ab0c0b5d357108
Jeanne <janetcs126@gmail.com>
2017-06-15 15:14:55 +0800

    Delete gulpfile.js

--- pagespeed/gulpfile.js
+++
@@ -1,9 +0,0 @@
-var gulp = require('gulp'),
-    connect = require('gulp-connect');
-gulp.task('connect', function() {
-  connect.server({
-    port: 80,
-    root: 'src',,
-    livereload: true 
-  });
-});
