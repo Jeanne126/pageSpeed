fe1e360e773595f3713a40323983328ddb407600
Jeanne <janetcs126@gmail.com>
2017-06-15 14:44:51 +0800

    Update main.js

--- pagespeed/views/js/main.js
+++ pagespeed/views/js/main.js
@@ -490,7 +490,7 @@ function updatePositions() {
   var items = document.querySelectorAll('.mover');
   var phase = Math.sin((document.body.scrollTop / 1250) + ((items.length-1)% 5));
   for (var i = 0; i < items.length; i++) {   
-    items[i].style.left =items[i].basicLeft+ 100 * phase + 'px';
+    items[i].style.left = items[i].basicLeft + 100 * phase + 'px';
   }
 
   // 再次使用User Timing API。这很值得学习
