f0af8cf93ff2688cffe181ad8707d33bb7ff115c
Jeanne <janetcs126@gmail.com>
2017-06-15 10:50:55 +0800

    Create main.js

--- pagespeed/views/js/main.js
+++ pagespeed/views/js/main.js
@@ -488,9 +488,10 @@ function updatePositions() {
   window.performance.mark("mark_start_frame");
 
   var items = document.querySelectorAll('.mover');
+  
   for (var i = 0; i < items.length; i++) {
     var phase = Math.sin((document.body.scrollTop / 1250) + (i % 5));
-    items[i].style.left = items[i].basicLeft + 100 * phase + 'px';
+    items[i].transform =translateX(100 * phase + 'px');
   }
 
   // 再次使用User Timing API。这很值得学习
@@ -510,7 +511,7 @@ window.addEventListener('scroll', updatePositions);
 document.addEventListener('DOMContentLoaded', function() {
   var cols = 8;
   var s = 256;
-  for (var i = 0; i < 200; i++) {
+  for (var i = 0; i < 100; i++) {
     var elem = document.createElement('img');
     elem.className = 'mover';
     elem.src = "images/pizza.png";
@@ -518,7 +519,7 @@ document.addEventListener('DOMContentLoaded', function() {
     elem.style.width = "73.333px";
     elem.basicLeft = (i % cols) * s;
     elem.style.top = (Math.floor(i / cols) * s) + 'px';
-    document.getElementById("#movingPizzas1").appendChild(elem);
+    document.querySelect("#movingPizzas1").appendChild(elem);
   }
   updatePositions();
 });
