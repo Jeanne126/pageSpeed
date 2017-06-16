381ccadd4ae173dfccb223d88b0a0676a5109a9d
Jeanne <janetcs126@gmail.com>
2017-06-16 14:49:41 +0800

    Update main.js

--- pagespeed/views/js/main.js
+++ pagespeed/views/js/main.js
@@ -396,7 +396,7 @@ var pizzaElementGenerator = function(i) {
 };
 
 // 当网站中"Our Pizzas"的滑窗部分移动时调用resizePizzas(size)函数
-var resizePizzas = function(size) {
+function resizePizzas(size) {
   window.performance.mark("mark_start_resize");   // User Timing API 函数
 
   // 改变滑窗前披萨的尺寸值
@@ -434,18 +434,15 @@ var resizePizzas = function(size) {
           console.log("bug in sizeSwitcher");
       }
     }
-
-
-
+changePizzaSizes(size);
   // 遍历披萨的元素并改变它们的宽度
  
     var randomPizzas=document.getElementsByClassName("randomPizzaContainer");
     for (var i = 0; i < randomPizzas.length; i++) {     
       randomPizzas[i].style.width = newWidth+"%";
-    }
+    } 
  
- changePizzaSizes(size);
-  resizePizzas(size);
+  
 
 
 
@@ -455,6 +452,7 @@ var resizePizzas = function(size) {
   var timeToResize = window.performance.getEntriesByName("measure_pizza_resize");
   console.log("Time to resize pizzas: " + timeToResize[timeToResize.length-1].duration + "ms");
 };
+resizePizzas(size);
 
 window.performance.mark("mark_start_generating"); // 收集timing数据
 
